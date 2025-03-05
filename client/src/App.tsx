4d068c20848af1442a5801bdb73976763e9dd4d7
mesesanb <bmesesan@gmail.com>
2025-03-05 11:44:49 +0200

    chore: FE initial setup with React, Typescript, Tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-nortonBackground text-nortonText min-h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-2xl mb-4 font-dos">Hello Norton!</h1>
+        <button className="bg-nortonButton text-nortonText py-2 px-6 rounded-none text-xl hover:bg-nortonText hover:text-nortonButton transition-colors font-dos">
+          Norton Commander Button
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Include toate fișierele relevante din folderul `src`
+  ],
+  theme: {
+    extend: {
+      colors: {
+        nortonBackground: '#0000a8',
+        nortonText: '#57ffff',
+        nortonButton: '#00a8a8',
+      },
+      fontFamily: {
+        dos: ['Dos', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
